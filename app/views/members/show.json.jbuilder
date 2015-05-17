6bbae8ce4c500053a1e762c7027e24776b2767d5
anand-mohanaveeramani <anand.mohanaveeramani@gmail.com>
2015-05-17 12:56:09 +0530

    addingm embers

---
+++ app/views/members/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @member, :id, :name, :deliver_rate, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150516174203) do
+ActiveRecord::Schema.define(version: 20150517072455) do
+
+  create_table "members", force: :cascade do |t|
+    t.string   "name",         limit: 255
+    t.decimal  "deliver_rate",             precision: 10
+    t.datetime "created_at",                              null: false
+    t.datetime "updated_at",                              null: false
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string   "name",       limit: 255
